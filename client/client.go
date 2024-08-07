b4351fbf58e6a533557f5f0a5b0ecb67d690e7d4
Daniel R-R <dannyrosalesrodr@u.boisestate.edu>
2024-08-07 17:17:49 -0600

    Client development
    implementing client side, will come back to it later

--- client/client.go
+++ client/client.go
@@ -1,3 +1,18 @@
 package client
 
-// rn
+import "bufio"
+
+type Client struct {
+	host string
+	port int
+}
+
+type Config struct {
+	Host   string
+	Client int
+}
+
+func (c *Client) dialer() {
+	var reader *bufio.Reader
+	var buf []byte
+}
